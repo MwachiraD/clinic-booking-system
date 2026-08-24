3fce41fbc17e72bd3852cc110402611d97ebe068
Dennis Mwangi <denochira78@gmail.com>
2026-08-24 16:24:44 +0300

    updated cancelling and rescheduling appointment logic

--- app/services/booking.py
+++ app/services/booking.py
@@ -120,3 +120,118 @@ def book_appointment(
 
     # 12. Return the appointment
     return appointment
+
+
+
+def cancel_appointment(
+    db: Session,
+    appointment_id: int,
+    cancellation_reason: str
+):
+    query = select(Appointment).where(
+        Appointment.id == appointment_id
+    )
+    
+    result = db.execute(query)
+    
+    appointment = result.scalar_one_or_none()
+    
+    if appointment is None:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Appointment not found"
+        )
+    if appointment.status == "cancelled":
+        raise HTTPException(
+            status_code = 400,
+            detail = "Appointment already cancelled"
+        )
+        
+    appointment.status = "cancelled"
+    appointment.cancellation_reason = cancellation_reason
+    
+    db.commit()
+    
+    db.refresh(appointment)
+    return appointment
+    
+    
+def reschedule_appointment(
+    db: Session,
+    appointment_id: int,
+    new_slot_start_time: datetime
+):
+    
+    query = select(Appointment).where(
+        Appointment.id == appointment_id
+    )
+    
+    result = db.execute(query)
+    appointment = result.scalar_one_or_none()
+    
+    if appointment is None:
+        raise HTTPException(
+            status_code = 404,
+            detail = "Appointment not found"
+        )
+        
+    if appointment.status != "confirmed":
+        raise HTTPException(
+            status_code = 400, 
+            detail = "only confirmed appointments can be rescheduled"
+        )
+        
+    min_booking_time = datetime.now() + timedelta(hours=1)
+
+    if new_slot_start_time < min_booking_time:
+        raise HTTPException(
+            status_code = 400,
+            detail="Rescheduled appointments must be at least 1 hour in advance"
+        )
+        
+    working_hours = get_working_hours(
+        db, 
+        appointment.doctor_id,
+        new_slot_start_time.date()
+    )
+    
+    valid_slots = generate_daily_slots(
+        new_slot_start_time.date(),
+        working_hours
+    )
+    
+    if new_slot_start_time not in valid_slots:
+        raise HTTPException(
+            status_code = 400,
+            detail="Slot is not within the doctor's working hours"
+        )
+        
+    query = select(Appointment).where(
+        Appointment.doctor_id == appointment.doctor_id,
+        Appointment.slot_start_time == new_slot_start_time,
+        Appointment.status == "confirmed",
+        Appointment.id != appointment_id
+    )
+    
+    result = db.execute(query)
+    existing_appointment = result.scalar_one_or_none()
+    
+    if existing_appointment is not None:
+        raise HTTPException(
+            status_code = 409, 
+            detail = "The slot is already booked. Please choose another time"
+        )
+            
+    appointment.slot_start_time = new_slot_start_time
+    
+    try:
+        db.commit()
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(
+            status_code = 409, 
+            detail = "This slot is already booked"
+        )
+    db.refresh(appointment)
+    
+    return appointment
